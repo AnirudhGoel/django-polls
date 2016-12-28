49f155bdffda0aa278d271575a91d474b7ec6734
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2016-12-28 23:37:40 +0530

    Optimized filenames

--- django-polls/urls.py
+++ django-polls/urls.py
@@ -1,4 +1,4 @@
-"""learndj URL Configuration
+"""django-polls URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- django-polls/wsgi.py
+++ django-polls/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for learndj project.
+WSGI config for django-polls project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learndj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-polls.settings")
 
 application = get_wsgi_application()
